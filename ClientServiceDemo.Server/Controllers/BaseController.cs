a3c2fd566283288b6054788a93fb9c309a562fbd
Shihab Khan <echoshihab@gmail.com>
2025-01-08 22:15:36 -0500

    add base controller

---
+++ ClientServiceDemo.Server/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientServiceDemo.Server.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public abstract class BaseController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id) 
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult Query() 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
