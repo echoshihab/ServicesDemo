217eb4a4d86a55ab51df50cff179c28af73c0925
Shihab Khan <echoshihab@gmail.com>
2025-02-03 22:57:24 -0500

    add vuetify and pinia

--- clientservicedemo.client/src/main.ts
+++ clientservicedemo.client/src/main.ts
@@ -1,6 +1,24 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createPinia } from "pinia";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const pinia = createPinia();
+
+const app = createApp(App);
+app.use(vuetify);
+app.use(pinia);
+
+app.mount("#app");
